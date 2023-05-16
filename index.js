41a88cded74a276d4faacd3474bdbd83c5bc3b8a
KristianWede <kris79s4@stud.kea.dk>
2023-05-16 15:09:00 +0200

    Final commit to "create account"

--- index.js
+++ index.js
@@ -5,18 +5,27 @@ import {  setActiveLink, adjustForMissingHash, renderTemplate, loadTemplate } fr
 
 import { testEverything } from "./pages/menuPage/test.js"
 import {initReceipts} from "./pages/recepter/recepter.js"
+import { initSignIn } from "./pages/signInPage/signInPage.js"
 
 window.addEventListener("load", async () => {
+
+    console.log("Trying to load page!")
 
     const templateTest = await loadTemplate("./pages/menuPage/test.html")
     const templateMenu = await loadTemplate("./pages/menu.html")
     const templateRecept = await loadTemplate("./pages/recepter/recepter.html")
+    const templateSignIn = await loadTemplate("./pages/signInPage/signInPage.html")
     
+
+    console.log("templates loaded!")
+
     adjustForMissingHash()
 
     const router = new Navigo("/", { hash: true });
 
     window.router = router
+
+    console.log("router loaded!")
    
     router
     .hooks({
@@ -27,24 +36,38 @@ window.addEventListener("load", async () => {
     })
     .on({
         //For very simple "templates", you can just insert your HTML directly like below
-        "/": () => document.getElementById("content").innerHTML =
+        "/": () => {document.getElementById("content").innerHTML =
           `<h2>Home</h2>
         <p style='margin-top:2em'>
         This is the content of the Home Route <br/>
         Observe that this is so simple that all HTML is added in the on-handler for the route. 
         </p>
-       `,
-        "/test": () => renderTemplate(templateTest, "content"),
-        "/menu": () => renderTemplate(templateMenu, "content"),
+       `
+       console.log("First page loaded!")}
+       ,
+        "/test": () => {
+          renderTemplate(templateTest, "content")
+          console.log("test loaded!")
+    },
+        "/menu": () => {
+          renderTemplate(templateMenu, "content")
+          console.log("Menu loaded!")
+        },
         
         "/recepter": () => { renderTemplate(templateRecept, "content")
         initReceipts()
+        },
+        "/signIn": () => { renderTemplate(templateSignIn, "content")
+        initSignIn()
         }
     })
     .notFound(() => {
       renderTemplate(templateNotFound, "content")
     })
     .resolve()
+
+    console.log("navigo loaded!")
+
 });
 
 window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
