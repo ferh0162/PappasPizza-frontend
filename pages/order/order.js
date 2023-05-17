ab1347443ba38905f306c31e4f548ea757cbd346
ferh0162 <ferh0162@stud.kea.dk>
2023-05-17 10:57:42 +0200

    cart bliver tom efter bestilling

--- pages/order/order.js
+++ pages/order/order.js
@@ -46,64 +46,64 @@ function saveUserInformation(event) {
     var payment = document.getElementById('payment').value;
     var name = firstname + " " + surname;
 
-   // Fetch the cart from local storage
-const storedCart = localStorage.getItem("cart");
-if (storedCart) {
-    const cart = JSON.parse(storedCart);
+    // Fetch the cart from local storage
+    const storedCart = localStorage.getItem("cart");
+    if (storedCart) {
+        const cart = JSON.parse(storedCart);
 
-    // Convert the cart items into the format expected by the server
-    const orderItems = cart.map(item => {
-        const addedIds = item.added ? item.added.map(ingredient => ingredient.id) : []; 
-        const removedIds = item.removed ? item.removed.map(ingredient => ingredient.id) : [];
-        let pizzaId = null;
-        let drinkId = null;
-        let pizzaTypeId = null;
+        // Convert the cart items into the format expected by the server
+        const orderItems = cart.map(item => {
+            const addedIds = item.added ? item.added.map(ingredient => ingredient.id) : []; 
+            const removedIds = item.removed ? item.removed.map(ingredient => ingredient.id) : [];
+            let pizzaId = null;
+            let drinkId = null;
+            let pizzaTypeId = null;
     
-        // Checking if ingredients are present, if not it's a drink
-        if(item.ingredients) {
-            pizzaId = item.id;
-            pizzaTypeId = item.pizzaTypeId; // assuming that pizzaTypeId is a property of item
-        } else {
-            drinkId = item.id;
-        }
+            // Checking if ingredients are present, if not it's a drink
+            if(item.ingredients) {
+                pizzaId = item.id;
+                pizzaTypeId = item.pizzaTypeId; // assuming that pizzaTypeId is a property of item
+            } else {
+                drinkId = item.id;
+            }
     
-        return {
-            pizzaId: pizzaId,
-            drinkId: drinkId,
-            quantity: item.quantity,
-            added: addedIds,
-            removed: removedIds,
-            pizzaTypeId: pizzaTypeId
+            return {
+                pizzaId: pizzaId,
+                drinkId: drinkId,
+                quantity: item.quantity,
+                added: addedIds,
+                removed: removedIds,
+                pizzaTypeId: pizzaTypeId
+            };
+        });
+
+        // Create the order request
+        const orderRequest = {
+            creationDate: new Date().toISOString(),
+            orderItems: orderItems,
+            phoneNumber: phone,
+            name: name,
+            address: address,
+            postalCode: zipcode,
+            pickUpTime: new Date().toISOString(), // assuming pickUpTime is current time
+            confirmed: false, // assuming order is confirmed on submission
+            status: 'FRESH' // assuming order status is fresh on submission
         };
-    });
-    
 
-console.log(orderItems)
-    // Create the order request
-    const orderRequest = {
-        creationDate: new Date().toISOString(),
-        orderItems: orderItems,
-        phoneNumber: phone,
-        name: name,
-        address: address,
-        postalCode: zipcode,
-        pickUpTime: new Date().toISOString(), // assuming pickUpTime is current time
-        confirmed: false, // assuming order is confirmed on submission
-        status: 'FRESH' // assuming order status is fresh on submission
-    };
-
-    // Send the order request to the server
-    fetch("http://localhost:8080/api/orders/addOrder", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(orderRequest)
-    }).then(response => response.json()).then(data => {
-        console.log("Order response", data);
-        // Redirect the user, display a confirmation message, etc.
-    }).catch(error => {
-        console.error("Error:", error);
-    });
-}
+        // Send the order request to the server
+        fetch("http://localhost:8080/api/orders/addOrder", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(orderRequest)
+        }).then(response => response.json()).then(data => {
+            console.log("Order response", data);
+            localStorage.removeItem('cart'); // Clear the cart
+            location.reload(); // Refresh the page
+            // Redirect the user, display a confirmation message, etc.
+        }).catch(error => {
+            console.error("Error:", error);
+        });
+    }
 }
